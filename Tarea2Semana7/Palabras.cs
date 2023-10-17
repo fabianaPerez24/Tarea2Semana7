ff5db200c18777aa16dfa2aa617e770094b8e05d
Fabiana Perez <fabianalertora003@gmail.com>
2023-10-17 12:12:22 -0500

    se edito Palabras y program

--- Tarea2Semana7/Palabras.cs
+++ Tarea2Semana7/Palabras.cs
@@ -8,6 +8,17 @@ namespace Tarea2Semana7
 {
     internal class Palabras
     {
+        public string resultado;
+        public string actual;
+        public Palabras(string texto)
+        {
+            this.resultado = texto;
+            this.actual = "";
 
+            for(int i = 0; i < texto.Length; i++)
+            {
+                this.actual += "_";
+            }
+        }
     }
 }
